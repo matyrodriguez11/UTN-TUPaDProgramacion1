91cc4bfaa2d87cfead91fbd7850a9a33cf067d89
matyrodriguez11 <94484395+matyrodriguez11@users.noreply.github.com>
2025-08-31 20:09:26 -0300

    corrección repo

--- pruebas.py
+++
@@ -1,5 +0,0 @@
-numero1 = float(input("Por favor ingrese un numero "))
-numero2 = float(input("Por favor ingrese otro numero "))
-numero3 = float(input("Por favor ingrese otro numero "))
-promedio = (numero1 + numero2 + numero3) / 3
-print(f"El promedio de los numeros ingresados es: {promedio}")
