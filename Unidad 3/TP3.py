c64e7f8d94eb22190b988e0d48b71520769681f5
matyrodriguez11 <94484395+matyrodriguez11@users.noreply.github.com>
2025-09-05 08:12:33 -0300

    Avance punto 10

--- Unidad 3/TP3.py
+++ Unidad 3/TP3.py
@@ -54,3 +54,85 @@ elif (media == mediana and mediana == moda):
     print("Sin sesgo")
 else:
     pass
+
+#Ejercicio 7
+frase = input("Por favor, ingrese una frase o palabra: ")
+ultimaLetra = frase[-1]
+if (ultimaLetra == "a" or ultimaLetra == "e" or ultimaLetra == "i" or ultimaLetra == "o" or ultimaLetra == "u"):
+    print(frase + "!")
+else:
+    print(frase)
+
+#Ejercicio 8
+nombre = input("Por favor, ingrese su nombre: ")
+opcion = int(input("""Por favor, elija una opcion ingresando 1, 2 o 3: 
+1. Si quiere su nombre en mayúsculas. Por ejemplo: PEDRO
+2. Si quiere su nombre en minúsculas. Por ejemplo: pedro
+3. Si quiere su nombre con la primera letra mayúscula. Por ejemplo: Pedro
+"""))
+if (opcion == 1):
+    print(nombre.upper())
+elif (opcion == 2):
+    print(nombre.lower())
+elif (opcion == 3):
+    print(nombre.title())
+else:
+    pass
+
+#Ejercicio 9
+magnitud = int(input("Por favor, ingrese en numeros la magnitud de un terremoto: "))
+if (magnitud < 3):
+    print("Muy leve (imperceptible)")
+elif (magnitud >= 3 and magnitud < 4):
+    print("Leve (ligeramente perceptible)")
+elif (magnitud >= 4 and magnitud < 5):
+    print( "Moderado (sentido por personas, pero generalmente no causa daños)")
+elif (magnitud >= 5 and magnitud < 6):
+    print("Fuerte (puede causar daños en estructuras débiles)")
+elif (magnitud >= 6 and magnitud < 7):
+    print("Muy Fuerte (puede causar daños significativos)")
+elif (magnitud >= 7):
+    print("Extremo (puede causar graves daños a gran escala)")
+else:
+    pass
+
+#Ejercicio 10
+hemisferio = input("¿En qué hemisferio te encuentras? (Norte/Sur): ")
+mes = int(input("Introduce el número de mes (1-12): "))
+dia = int(input("Introduce el día del mes (1-31): "))
+hemisferio = hemisferio.lower()
+
+
+def hemisferioNorte(hemisferio) :
+    if (mes == 12 and dia >= 21) or (mes in [1, 2]) or (mes == 3 and dia <= 20):
+    estacion_norte = "Invierno"
+    estacion_sur = "Verano"
+    elif (mes == 3 and dia >= 21) or (mes in [4, 5]) or (mes == 6 and dia <= 20):
+    estacion_norte = "Primavera"
+    estacion_sur = "Otoño"
+    elif (mes == 6 and dia >= 21) or (mes in [7, 8]) or (mes == 9 and dia <= 20):
+    estacion_norte = "Verano"
+    estacion_sur = "Invierno"
+    elif (mes == 9 and dia >= 21) or (mes in [10, 11]) or (mes == 12 and dia <= 20):
+    estacion_norte = "Otoño"
+    estacion_sur = "Primavera"
+    else:
+    return "Fecha inválida"
+    return estacion_norte if hemisferio == "n" else estacion_sur
+
+def hemisferioSur(hemisferio) :
+    if (mes == 12 and dia >= 21) or (mes in [1, 2]) or (mes == 3 and dia <= 20):
+    estacion_norte = "Invierno"
+    estacion_sur = "Verano"
+    elif (mes == 3 and dia >= 21) or (mes in [4, 5]) or (mes == 6 and dia <= 20):
+    estacion_norte = "Primavera"
+    estacion_sur = "Otoño"
+    elif (mes == 6 and dia >= 21) or (mes in [7, 8]) or (mes == 9 and dia <= 20):
+    estacion_norte = "Verano"
+    estacion_sur = "Invierno"
+    elif (mes == 9 and dia >= 21) or (mes in [10, 11]) or (mes == 12 and dia <= 20):
+    estacion_norte = "Otoño"
+    estacion_sur = "Primavera"
+    else:
+    return "Fecha inválida"
+    return estacion_norte if hemisferio == "n" else estacion_sur    
