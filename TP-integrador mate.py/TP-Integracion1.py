3c815ba0851d58ada9c8d470f2c22092e5d86834
matyrodriguez11 <94484395+matyrodriguez11@users.noreply.github.com>
2025-09-26 23:05:05 -0300

    Create TP-Integracion1.py

---
+++ TP-integrador mate.py/TP-Integracion1.py
@@ -0,0 +1,18 @@
+import time
+
+#Declaramos la lista
+numeros_Binarios = ["0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"]
+
+#Inicializamos el contador
+contador = 0
+
+#Comenzamos el programa
+print("Contamos de 0 a 15 en binario:\n")
+
+for numero in numeros_Binarios: #Recorremos toda la lista
+    print(contador, "=", numero) 
+    contador+= 1 #Incrementamos el contador
+    time.sleep(1.25) #Retraso de un segundo
+
+#Finalizamos el programa
+print("\nFin del programa") 
