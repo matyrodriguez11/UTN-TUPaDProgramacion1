742ffc1d1018723901c6f39dda9eda90d9a4b3e8
matyrodriguez11 <94484395+matyrodriguez11@users.noreply.github.com>
2025-10-01 15:45:10 -0300

    avance

---
+++ Prueba.py
@@ -0,0 +1,7 @@
+#Ejercicio 3
+inicial = int(input("Por favor ingresa un numero entero inicial"))
+final = int(input("Por favor ingresa un numero entero final"))
+sumatoria = 0
+for i in range ((inicial+1), (final)):
+    sumatoria += i
+print(sumatoria) 
--- Unidad 4/TP4.py
+++ Unidad 4/TP4.py
@@ -3,4 +3,18 @@ for i in range(0,101):
     print(i)
 
 #Ejercicio 2
-numero = int(input("Por favor ingrese un numero entero"))
+numero = int(input("Por favor ingrese un numero entero: "))
+contador = 0
+while numero > 10:
+    numero = numero // 10 
+    contador += 1
+print(f"El numero tiene {contador} digitos")
+
+#Ejercicio 3
+inicial = int(input("Por favor ingresa un numero entero inicial"))
+final = int(input("Por favor ingresa un numero entero final"))
+sumatoria = 0
+for i in range ((inicial+1), (final)):
+    sumatoria += i
+print(sumatoria)
+
